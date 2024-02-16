6472e5d69fc401a53e143f3eb2afe39655db3d6b
Jagadeesh <97141186+Jagadeesh-Udugundla@users.noreply.github.com>
2024-02-16 20:36:04 +0530

    Initial commit

---
+++ DataSchema.js
@@ -0,0 +1,40 @@
+const mongoose =require("mongoose")
+// const Schema= new mongoose.Schema({
+//     image:{
+//         type:String,
+//         required:true
+//     },
+//     price:{
+//         type:Number,
+//         required:true
+//     },
+//     discount:{
+//         type:Number,
+//         required:true
+//     },
+//     name:{
+//         type:String,
+//         required:true
+//     }
+// })
+// module.exports=mongoose.model("Data",Schema)
+const Schema = new mongoose.Schema({
+    image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    discount: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports=mongoose.model("Data", Schema);
