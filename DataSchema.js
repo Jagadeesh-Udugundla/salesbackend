d39c87b108fc93ae8d60c102d9f74eb1680ae211
Jagadeesh <97141186+Jagadeesh-Udugundla@users.noreply.github.com>
2024-02-16 20:41:16 +0530

    Done changes

--- DataSchema.js
+++ DataSchema.js
@@ -1,23 +1,4 @@
 const mongoose =require("mongoose")
-// const Schema= new mongoose.Schema({
-//     image:{
-//         type:String,
-//         required:true
-//     },
-//     price:{
-//         type:Number,
-//         required:true
-//     },
-//     discount:{
-//         type:Number,
-//         required:true
-//     },
-//     name:{
-//         type:String,
-//         required:true
-//     }
-// })
-// module.exports=mongoose.model("Data",Schema)
 const Schema = new mongoose.Schema({
     image: {
         type: String,
